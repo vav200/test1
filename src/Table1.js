71069a78b21a6583d592043cd9946dc33e9b7e62
vav200 <vav200@gmail.com>
2023-10-27 16:14:16 +0300

    cor1

--- src/Table1.js
+++ src/Table1.js
@@ -1,11 +1,15 @@
+import { useEffect } from "react";
+
 export default function Table1() {
   function gettable() {
-    fetch("https://technical-task-api.icapgroupgmbh.com/api/table/")
-      .then((data) => data.json())
-      .then((data) => {
-        console.log(data);
-        data.results.map((item) => <li>{item.name}</li>);
-      });
+    useEffect(() => {
+      fetch("https://technical-task-api.icapgroupgmbh.com/api/table/")
+        .then((data) => data.json())
+        .then((data) => {
+          console.log(data);
+          // data.results.map((item) => <li>{item.name}</li>);
+        });
+    });
   }
   return (
     <>
