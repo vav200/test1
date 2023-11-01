b8cb07cdb64e12d1b4df939f0b4ebf9e0962a6a6
vav200 <vav200@gmail.com>
2023-11-01 09:13:51 +0200

    kor7

--- src/Table1.js
+++ src/Table1.js
@@ -10,7 +10,7 @@ export default function Table1() {
   // });
 
   function getData() {
-    fetch("https://technical-task-api.icapgroupgmbh.com/api/table/")
+    return fetch("https://technical-task-api.icapgroupgmbh.com/api/table/")
       .then((data) => data.json())
       .then((data) => {
         console.log(data.results);
