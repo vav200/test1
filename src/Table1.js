178e7263aafc44c9402a774798c5fa0a43595db7
vav200 <vav200@gmail.com>
2023-11-01 08:27:53 +0200

    добавлена таблица

--- src/Table1.js
+++ src/Table1.js
@@ -1,18 +1,32 @@
 import { useEffect } from "react";
 
 export default function Table1() {
-  useEffect(() => {
+  // useEffect(() => {
+  //   fetch("https://technical-task-api.icapgroupgmbh.com/api/table/")
+  //     .then((data) => data.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     });
+  // });
+
+  function getData() {
     fetch("https://technical-task-api.icapgroupgmbh.com/api/table/")
       .then((data) => data.json())
       .then((data) => {
         console.log(data);
-        // data.results.map((item) => <li>{item.name}</li>);
+        data.results.map((item) => (
+          <tr>
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+          </tr>
+        ));
       });
-  });
+  }
 
   return (
     <>
       <h1>Таблица</h1>
+      <table>{getData}</table>
     </>
   );
 }
